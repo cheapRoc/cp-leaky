e8084016ae6c9312a339fe9e1b40cff87ac8e87d
Justin Reagor <justin.reagor@joyent.com>
2018-04-22 20:10:43 -0400

    Refactor test shell script to get to working condition

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 now=$(date +%s)
+tests=(
+    job-only
+    job-that-logs
+    job-with-health-check
+    job-with-service
+    watch-only
+    watch-trigger-job
+)
 
 run-with-config() {
     name=$1
@@ -8,17 +16,20 @@ run-with-config() {
            --name "$name" \
            -v "$(pwd):/minimal" \
            --link containerpilot_consul:consul \
-           leaky \
-           /minimal/containerpilot -config "/minimal/${name}.json5"
+           -e CP_PROFILING=1 \
+           containerpilot_base \
+           /minimal/containerpilot -config "/minimal/etc/${name}.json5"
 }
 
 do-pmap() {
+    printf "\tSampling pmap for $name\n"
     name=$1
     pid=$(docker exec -it "$name" pgrep containerpilot | tail -1 | tr -d '[:space:]')
     docker exec -it "$name" pmap -x "$pid" > "pmaps/${name}-$now"
 }
 
 do-profile() {
+    printf "\tSampling pprof profile for $name\n"
     name=$1
     docker exec -it "$name" \
            curl -so "/minimal/profiles/heap-${name}-${now}" \
@@ -26,54 +37,47 @@ do-profile() {
 }
 
 do-goroutine() {
+    printf "\tSampling goroutine stacks for $name\n"
     name=$1
     docker exec -it "$name" \
-           curl -so "/minimal/profiles/goroutine-${name}-${now}" \
+           curl -so "/minimal/goroutines/goroutine-${name}-${now}" \
            "http://localhost:6060/debug/pprof/goroutine?debug=1"
 }
 
-run() {
-    docker rm -f containerpilot_consul > /dev/null 2>&1 || true
+start() {
+    stop
+    docker build -t containerpilot_base .
     docker run -d -m 256m -p 8500:8500 --name containerpilot_consul \
            consul:latest agent -dev -client 0.0.0.0 -bind=0.0.0.0
-    # run-with-config do-nothing
-    run-with-config job-only
-    run-with-config job-that-logs
-    run-with-config job-with-health-check
-    run-with-config job-with-service
-    run-with-config watch-only
-    run-with-config watch-trigger-job
+
+    for name in "${tests[@]}"; do
+        run-with-config $name
+    done
 }
 
-profiles() {
+stop() {
+    for name in "${tests[@]}"; do
+        echo "--> Stopping $name"
+        docker rm -f $name > /dev/null 2>&1 || true
+    done
+
+    docker rm -f containerpilot_base > /dev/null 2>&1 || true
+    docker rm -f containerpilot_consul > /dev/null 2>&1 || true
+}
+
+profile() {
     mkdir -p profiles
     mkdir -p pmaps
+    mkdir -p goroutines
     while true; do
         now=$(date +%s)
-
-        # do-profile do-nothing
-        do-profile job-only
-        do-profile job-that-logs
-        do-profile job-with-health-check
-        do-profile job-with-service
-        do-profile watch-only
-        do-profile watch-trigger-job
-
-        # do-goroutine do-nothing
-        do-goroutine job-only
-        do-goroutine job-that-logs
-        do-goroutine job-with-health-check
-        do-goroutine job-with-service
-        do-goroutine watch-only
-        do-goroutine watch-trigger-job
-
-        # do-pmap do-nothing
-        do-pmap job-only
-        do-pmap job-that-logs
-        do-pmap job-with-health-check
-        do-pmap job-with-service
-        do-pmap watch-only
-        do-pmap watch-trigger-job
+        for name in "${tests[@]}"; do
+            echo "--> Measuring $name"
+            do-profile $name
+            do-goroutine $name
+            do-pmap $name
+        done
+        echo "--> Sleeping for 5 minutes"
         sleep 300
     done
 }
